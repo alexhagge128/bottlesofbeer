6a948d51ca0ee7432442187eab63126abd098f90
Alex Hagge <alexhagge128@gmail.com>
2016-03-02 18:00:51 -0800

    added 99 bottles of beer app

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,27 @@
-var pigLatin = function(englishWord) {
-  var splitAt = indexOfFirstVowel(englishWord);
-  return englishWord.slice(splitAt) + englishWord.slice(0, splitAt) + 'ay';
-};
-
-var indexOfFirstVowel = function(word) {
-  var vowels = ['a', 'e', 'i', 'o', 'u'];
-  var letters = word.split("");
-
-  for (var i = 0; i < letters.length; i++) {
-    for (var j = 0; j < vowels.length; j++) {
-      if (letters[i] === vowels[j]) {
-        return i;
-      }
-    }
-  }
-};
-
 $(document).ready(function() {
-  $("form#pigForm").submit(function(event) {
-    var englishWord = $("input#english").val();
+  $("form#beerForm").submit(function(event) {
+    var userNumber = $("input#beer").val();
 
-    var translation = pigLatin(englishWord);
-
-    alert(translation);
+  var bottles;
+  for (counter = userNumber; counter >= 1; counter = counter - 1)
+  {
+      if (counter == 1) {
+          bottles = 'bottle';
+      } else {
+          bottles = 'bottles';
+      }
+      alert(counter+" "+bottles+" of beer on the wall.");
+      if (counter < userNumber) {
+          alert("");
+          alert(counter+" "+bottles+" of beer on the wall.");
+      }
+      alert(counter+" "+bottles+" of beer.");
+      alert("Take one down.");
+      alert("Pass it around.");
+      if (counter == 1) {
+          alert("No bottles of beer on the wall.");
+      }
+  }
 
     event.preventDefault();
   });
